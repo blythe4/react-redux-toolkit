1bbf0c50db8a04561ae65da17016dd3740bf20cc
황수영 <blythe@jstair.com>
2023-07-03 14:34:13 +0900

    Change store file location

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./components/App";
-import countStore from "./components/store";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={countStore}>
+    <Provider store={store}>
         <App />
     </Provider>
 );
