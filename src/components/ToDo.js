e56522ab29336399965d3eff546ba1c7500c505c
황수영 <blythe@jstair.com>
2023-07-03 17:47:14 +0900

    mapStateToProps ownProps empty -> useSelector, useDispatch

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,10 +1,12 @@
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { deleteToDo } from "../store";
 
-const Todo = ({ text, onBtnClick }) => {
+const Todo = ({ text, onBtnClick, id }) => {
     return (
         <li>
-            {text} <button onClick={onBtnClick}>DEL</button>
+            <Link to={`/${id}`}>{text}</Link>
+            <button onClick={onBtnClick}>DEL</button>
         </li>
     );
 };
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteToDo } from "../store";
+
 const Detail = () => {
-    return <div>Detail</div>;
+    const navigate = useNavigate();
+    const id = useParams().id;
+    const toDo = useSelector((state) => state.find((toDo) => toDo.id === parseInt(id)));
+    const dispatch = useDispatch();
+    const onBtnClick = () => {
+        dispatch(deleteToDo(id));
+        navigate("/", { replace: true });
+    };
+    return (
+        <>
+            <h1>{toDo?.text}</h1>
+            <h5>Created at: {toDo?.id}</h5>
+            <div>
+                <button onClick={onBtnClick}>DEL</button>
+            </div>
+        </>
+    );
 };
+
 export default Detail;
